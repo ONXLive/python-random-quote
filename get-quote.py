03a10cda567ae7962f352482083be92214aea6fc
onyxcode <talk@onyxcode.space>
2020-07-03 00:21:05 -0400

    Finished lol

--- get-quote.py
+++ get-quote.py
@@ -2,9 +2,11 @@ import random
 def primary():
   print("Keep it logically awesome.")
 
+  f = open("quotes.txt", "a")
+  f.write(input("Tell me something I don't know: " + "\n"))
+  f.close()
   f = open("quotes.txt")
   quotes = f.readlines()
-  f.close()
   last = 13
   rnd = random.randint(0, last)
   print(quotes[rnd])
